acabf32ab396107f43f867909fdaf063ba000145
AMRITANSHU-SINGH1 <amritanshusk2610129@gmail.com>
2026-04-27 16:35:14 +0530

    changed the admin password

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@ from app import create_app
 from extensions import db
 from models import User
 from werkzeug.security import generate_password_hash
-
+import os
 app = create_app()
 
 def init_db():
@@ -14,15 +14,16 @@ def init_db():
         # Check if super admin exists
         admin = User.query.filter_by(username='admin').first()
         if not admin:
+            admin_password = os.environ.get('ADMIN_PASSWORD', 'admin123')
             admin = User(
                 username='admin',
                 email='admin@carbonx.com',
-                password_hash=generate_password_hash('admin123'),
+                password_hash=generate_password_hash(admin_password),
                 role='admin'
             )
             db.session.add(admin)
             db.session.commit()
-            print("Super Admin created. Credentials -> admin / admin123")
+            print(f"Super Admin created. Credentials -> admin / {admin_password}")
         else:
             print("Super Admin already exists.")
 
